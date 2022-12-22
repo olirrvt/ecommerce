2d78be1ac088253486a8ce6919c15f3a247c78f9
Taylor dos Santos Oliveira <taylor.oliveira@tcm.ba.gov.br>
2022-12-22 10:13:24 -0300

    Iniciando o Header e a Home

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,14 +1,19 @@
 import "./Home.css";
+import Cabecalho from "../../components/Header/Cabecalho";
 
 const Home = () => {
     return (
-        <>
+        <div className="conteudo">  
+
+            <Cabecalho/>
+
             <section>
                 <div>
                     Página principal
                 </div>
             </section>
-        </>
+
+        </div>
     );
 };
 
