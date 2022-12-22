832b10baf44ffef2d91f9a166ef88739b47abc37
Taylor dos Santos Oliveira <taylor.oliveira@tcm.ba.gov.br>
2022-12-22 11:42:11 -0300

    Reajuste no Header

--- src/components/Header/Cabecalho.jsx
+++ src/components/Header/Cabecalho.jsx
@@ -4,26 +4,30 @@ import lupa from "../../assets/pesquisa-de-lupa.png";
 
 const Cabecalho = () => {
   return (
-    <div className="container-caixa-header">
+      <div className="container-caixa-header">
 
-      <div className="container-logo">
-        <h2 className="titulo-header">TechCommerce</h2>
-        <img src={logo} alt="logo-da-pagina" className="logo-da-pagina" />
-      </div>
-
-      <div className="container-pesquisar">
-        <input className="input-pesquisar-header" type="text" placeholder="Pesquise aqui o produto que deseja..."/>
-        <div className="caixa-lupa-pesquisar">
-            <img className="lupa-input" src={lupa} alt="lupa-pesquisar" />
+        <div className="container-logo">
+          <h2 className="titulo-header">TechCommerce</h2>
+          <img src={logo} alt="logo-da-pagina" className="logo-da-pagina" />
         </div>
-      </div>
 
-      <div className="container-usuario">
-        <button className="btn-login">Sign In</button>
-        <button className="btn-cadastrar">Sign Up</button>
-      </div>
+        <div className="container-pesquisar">
+          <input
+            className="input-pesquisar-header"
+            type="text"
+            placeholder="Pesquise aqui o produto que deseja..."
+          />
+          <div className="caixa-lupa-pesquisar">
+            <img className="lupa-input" src={lupa} alt="lupa-pesquisar" />
+          </div>
+        </div>
 
-    </div>
+        <div className="container-usuario">
+          <button className="btn-login">Sign In</button>
+          <button className="btn-cadastrar">Sign Up</button>
+        </div>
+  
+      </div>
   );
 };
 
