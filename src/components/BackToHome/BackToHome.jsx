c4bbc9bdae05d3fe37d5f56debfcebe725832926
Taylor dos Santos Oliveira <taylor.oliveira@tcm.ba.gov.br>
2023-01-04 11:13:23 -0300

    Construindo a tela de login

---
+++ src/components/BackToHome/BackToHome.jsx
@@ -0,0 +1,12 @@
+import "./BackToHome";
+import { Link } from "react-router-dom";
+
+const BackToHome = () => {
+    return (
+        <>
+            <Link></Link>
+        </>
+    );
+};
+
+export default BackToHome;
--- src/pages/Login/Login.jsx
+++ src/pages/Login/Login.jsx
@@ -1,14 +1,55 @@
+import "./Login.css";
+import login from "../../assets/impressao-digital.png"
 // import { useState, useEffect } from "react";
 
 const Login = () => {
-    return (
-        <div>
-            <div>
-                Tela de Login
-            </div>
-        </div>
-    );
-;}
+  return (
+    <body>
+      <div>
 
+        <main>
+
+          <div className="container-login">
+
+            <div className="caixa-login">
+
+              <div className="conteudo-login">
+                <img className="login-icon" src={ login } alt="icone-de-login" />
+                <h3 className="titulo-login">Faça o seu Login</h3>
+              </div>
+
+              <div>
+                <form className="formulario-login">
+                  <input 
+                  className="input-name"
+                  placeholder="Usuário"
+                  type="text" 
+                  name="nome-user" 
+                  id="id-username" 
+                  />
+                  <input
+                  className="input-senha"
+                  placeholder="Senha"
+                    type="password"
+                    name="senha-user"
+                    id="id-userpassword"
+                  />
+
+                  <button className="btn-login-formulario">Entrar</button>
+
+                </form>
+
+              </div>
+
+            </div>
+
+          </div>
+
+        </main>
+
+      </div>
+    </body>
+  );
+};
 
 export default Login;
