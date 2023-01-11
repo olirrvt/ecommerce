6b9dc6013c89ebc0aa83fcd0c87ffe1c284f6a25
Taylor dos Santos Oliveira <taylor.oliveira@tcm.ba.gov.br>
2023-01-11 10:13:03 -0300

    Loading no POST cadastro

--- src/pages/Cadastro/Cadastro.jsx
+++ src/pages/Cadastro/Cadastro.jsx
@@ -10,126 +10,142 @@ import { useState } from "react";
 const url = "http://localhost:4000/newUser";
 
 const Cadastro = () => {
+  // Loading
+  const [load, setLoad] = useState(false);
+  // Valores do Input
+  const [nome, setNome] = useState("");
+  const [email, setEmail] = useState("");
+  const [senha, setSenha] = useState("");
+  // POST Reply
+  const [resposta, setResposta] = useState(null);
 
-    const [ nome, setNome ] = useState("");    
-    const [ email, setEmail ] = useState("");    
-    const [ senha, setSenha] = useState("");
-    // POST Reply
-    const [resposta, setResposta] = useState(null);
+  const handleNome = (e) => {
+    // Validation
+    setNome(e.target.value);
+  };
 
-    const handleNome = (e) => {
-        // Validation
-        setNome(e.target.value);
-    };
+  const handleEmail = (e) => {
+    // Validation
+    setEmail(e.target.value);
+  };
 
-    const handleEmail = (e) => {
-        // Validation
-        setEmail(e.target.value);
-    };
+  const handleSenha = (e) => {
+    // Validation
+    setSenha(e.target.value);
+  };
 
-    const handleSenha = (e) => {
-        // Validation
-        setSenha(e.target.value);
-    };
+  const handleSubmit = async (e) => {
 
-    const handleSubmit = (e) => {
-        e.preventDefault();
+    e.preventDefault();
 
-        const user = {
-            nome,
-            email,
-            senha
-        }
+    setLoad(true);
 
-        const httpPost = async (user) => {
-            const res = await fetch(url, {
-                method: "POST",
-                headers: {
-                    "Content-Type": "application/json",
-                },
-                body: JSON.stringify(user),
-            })
-            .then(res => res.json())
-            .then((res) => {
-                setResposta(res);
-            })
-        };
+    // FORM em OBJETO
+    const user = {
+      nome,
+      email,
+      senha,
+    }; 
 
-        httpPost(user);
-        
-    };
+    // Enviando Dados do Cadastro
 
-    
-    return (
-        <>
+    setLoad(true);
 
-            <BackToHome />
+      const res = await fetch(url, {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify(user),
+      })
+        .then((res) => res.json())
+        .then((res) => {
+          setResposta(res);
+        });
 
-            <main>
+    setLoad(false);
 
-                <div className="container-cadastro">
+    // Esvaziando os Inputs 
 
-                    <div className="caixa-cadastro">
+    setNome("");
+    setEmail("");
+    setSenha("");
 
-                        <div className="container-conteudo-cadastro">
-                            <h2 className="titulo-escrito-cadastro">Faça o seu cadastro!</h2>
-                        </div>
+  };
 
-                        <div className="container-formulario">
 
-                            <form onSubmit={handleSubmit} className="formulario-cadastro">
+  return (
+    <>
+      <BackToHome />
 
-                                <label>
-                                    <input
-                                    className="input-cadastro"
-                                    type="text"
-                                    name="nome-user"
-                                    id="username"
-                                    placeholder="Insira o seu nome de usuário..."
-                                    onChange={handleNome}
-                                    value={ nome || "" }
-                                    />
-                                </label>
+      <main>
+        <div className="container-cadastro">
+          <div className="caixa-cadastro">
+            <div className="container-conteudo-cadastro">
+              <h2 className="titulo-escrito-cadastro">Faça o seu cadastro!</h2>
+            </div>
 
-                                <label>
-                                    <input
-                                    className="input-cadastro"
-                                    type="email"
-                                    name="email-user"
-                                    id="emailUser"
-                                    placeholder="Insira o seu email..."
-                                    onChange={handleEmail}
-                                    value={ email || "" } 
-                                    />
-                                </label>
+            <div className="container-formulario">
 
-                                <label>
-                                    <input
-                                    className="input-cadastro"
-                                    type="password"
-                                    name="senha-user"
-                                    id="password"
-                                    placeholder="Insira a sua senha..."
-                                    onChange={handleSenha}
-                                    value={ senha || "" }
-                                    />
-                                </label>
+              <form onSubmit={handleSubmit} className="formulario-cadastro">
+                <label>
+                  <input
+                    className="input-cadastro"
+                    type="text"
+                    name="nome-user"
+                    id="username"
+                    placeholder="Insira o seu nome de usuário..."
+                    onChange={handleNome}
+                    value={nome || ""}
+                  />
+                </label>
 
-                                <div className="container-btn-cadastrar">
-                                    <button className="btn-cadastro" type="submit">Cadastrar</button>
-                                    <span className="span-link">Já possui cadastro? <Link to="/login" className="link-login" >Entrar</Link> </span>
-                                </div>
+                <label>
+                  <input
+                    className="input-cadastro"
+                    type="email"
+                    name="email-user"
+                    id="emailUser"
+                    placeholder="Insira o seu email..."
+                    onChange={handleEmail}
+                    value={email || ""}
+                  />
+                </label>
 
-                            </form>
-                        </div>
+                <label>
+                  <input
+                    className="input-cadastro"
+                    type="password"
+                    name="senha-user"
+                    id="password"
+                    placeholder="Insira a sua senha..."
+                    onChange={handleSenha}
+                    value={senha || ""}
+                  />
+                </label>
 
-                    </div>
+                {!load && (
+                  <div className="container-btn-cadastrar">
+                    <button className="btn-cadastro" type="submit">
+                      Cadastrar
+                    </button>
+                    <span className="span-link">
+                      Já possui cadastro?{" "}
+                      <Link to="/login" className="link-login">
+                        Entrar
+                      </Link>{" "}
+                    </span>
+                  </div>
+                )}
+              </form>
 
-                    </div>
+            </div>
+          </div>
+        </div>
 
-            </main>
-        </>
-    );
+      </main>
+    </>
+  );
 };
 
 export default Cadastro;
