c7838993c5f06545550ae07aa09fc7f7e1e1c229
Taylor dos Santos Oliveira <taylor.oliveira@tcm.ba.gov.br>
2023-01-19 11:17:47 -0300

    Finalizando Card Home

---
+++ src/components/CardHome/CardHome.jsx
@@ -0,0 +1,62 @@
+// CSS
+import { useEffect, useState } from "react";
+import "./CardHome.css";
+
+const CardHome = (items) => {
+
+  const [produto, setProduto] = useState([]);
+  const [limitProd, setLimitProd] = useState([]);
+
+  useEffect(() => {
+
+    const prodArray = produto.map((prod) => {
+        return prod;
+    });
+
+    const limitProd = prodArray.filter((prodFilt) => {
+        return prodFilt.id <= 4;
+    });
+
+    setLimitProd(limitProd)
+
+  }, [produto]);
+
+  useEffect(() => {
+    items.items == null ? null : setProduto(items.items.produto);
+  }, [items]);
+
+  return (
+    <>
+      <div className="container-card-home">
+        { limitProd &&
+          limitProd.map((prod) => (
+            <div key={prod.id} className="card-home">
+              <div className="card-home-header">
+                <div className="anuncio-escrito">
+                  <h4>23% OFF</h4>
+                </div>
+                <div className="promocao-escrito">
+                  <h5>Summer Party</h5>
+                </div>
+              </div>
+
+              <div className="card-home-body">
+                <div>
+                  <h2>{prod.titulo}</h2>
+                </div>
+              </div>
+
+              <div className="card-home-footer">
+                <div className="desc-prod">
+                  {prod.descricao}
+                </div>
+                <div className="valor-prod">R$ {prod.valor} à vista</div>
+              </div>
+            </div>
+          ))}
+      </div>
+    </>
+  );
+};
+
+export default CardHome;
--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,10 +1,21 @@
+// CSS
 import "./Home.css";
+// Components
 import Cabecalho from "../../components/Header/Cabecalho";
 import Banner from "../../components/Banner/Banner";
 import Footer from "../../components/Footer/Footer";
+import CardHome from "../../components/CardHome/CardHome";
+// Custom Hook
+import { useFetch } from "../../hooks/useFetch";
+// React
 import { useEffect, useState } from "react";
+// URL
+const url = "http://localhost:4000/products";
+
 
 const Home = () => {
+
+  const { dados: items, loading } = useFetch(url);
 
   return (
     <>
@@ -15,6 +26,9 @@ const Home = () => {
       </section>
 
       <main>
+        <div className="card-container-principal">
+          <CardHome items={items} />
+        </div>
       </main>
 
       <Footer />
