acb27817a1a289bb0d35ae7ba660b1cf18de9847
Taylor Oliveira <tayloroliveira@MBP-de-Taylor.local>
2023-01-25 07:01:28 -0300

    PUT method Axios

--- src/services/editUser.js
+++ src/services/editUser.js
@@ -3,37 +3,52 @@ import axios from "axios";
 
 // Find User Email
 const findUser = async () => {
-    // Find User ID
-    const email = localStorage.getItem("email");
-    const url = `http://localhost:4000/usermail/${email}`;
+  // Find User ID
+  const email = localStorage.getItem("email");
+  const url = `http://localhost:4000/usermail/${email}`;
 
-    const res = await axios.get(url);
+  const res = await axios.get(url);
 
-    return res;
-}
+  return res;
+};
 
 const editUser = async (nome, email, senha) => {
-
+  try {
     const resFunction = await findUser();
     const user = resFunction.data.usuario;
 
-    // Usuario Existente  
-    const idUser = user[0].id;
+    // Usuario Existente
+    const userId = user[0].id;
+    // URL
+    const url = `http://localhost:4000/user/${userId}`;
 
-    console.log(nome, email, senha);
+    // Filtro de Dados
+    const nomeUser = nome || user[0].nome;
+    const emailUser = email || user[0].email;
+    const senhaUser = senha || user[0].senha;
 
-    // Dados Antigos
-    const nomeUser = user[0].nome;
-    const emailUser = user[0].email;
-    const senhaUser = user[0].senha;
+    const data = {
+      nomeUser,
+      emailUser,
+      senhaUser,
+    };
 
-    // console.log(nomeUser, emailUser, senhaUser)
+    // Fetch Config
 
-    const url = `http://localhost:4000/user/${idUser}`
+    const res = await fetch(url, {
+      method: "PUT",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(data),
+    });
 
-    // axios.put(url, {
-        
-    // })
-}
+    console.log(res);
+    return res;
+
+  } catch(erro) {
+    console.log(erro);
+  }
+};
 
 export default editUser;
